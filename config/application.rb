6be51ecfb778e5572e32cb15474f79956118c8e4
mduarth <mduarth@hotmail.com>
2016-03-26 20:23:01 +0000

    Adicionado Helper

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,9 @@ module SampleApp
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.web_console.whitelisted_ips = '10.0.2.2'
+    # config.web_console.whitelisted_ips = '10.0.2.2'
+
     config.active_record.raise_in_transactional_callbacks = true
+    
   end
 end
